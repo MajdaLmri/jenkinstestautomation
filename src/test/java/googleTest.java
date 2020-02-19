2f44389fb95c858a214821ebe7a9cbe55cc5125d
MajdaLmri <laamari.majda@gmail.com>
2020-02-19 15:37:16 +0100

    expl test auto

---
+++ src/test/java/googleTest.java
@@ -0,0 +1,38 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class googleTest {
+    public String baseUrl = "https://www.google.com/";
+    String driverPath = "/Users/majdalaamai/IdeaProjects/AutoTest1/src/test/resources/chromedriver";
+    public WebDriver driver ;
+
+    @BeforeTest
+    public void setup(){
+        System.out.println("launching Chrome browser");
+        System.setProperty("webdriver.chrome.driver", driverPath);
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void verifyHomepageTitle() {
+
+        driver.get(baseUrl);
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Assert.assertEquals(true, driver.findElement(By.id("hplogo")).isDisplayed());
+
+    }
+
+    @AfterTest
+    public void teardown(){
+        System.out.println("test passed");
+        driver.close();
+    }
+
+}
