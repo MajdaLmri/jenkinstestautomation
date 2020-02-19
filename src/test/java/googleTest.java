e3811d7f9e71af9c613f0df3ea0d2ea93c8e6f81
MajdaLmri <laamari.majda@gmail.com>
2020-02-19 16:09:02 +0100

    expl test auto

--- src/test/java/googleTest.java
+++ src/test/java/googleTest.java
@@ -1,10 +1,11 @@
+import junit.framework.TestCase;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
-public class googleTest {
+public class googleTest extends TestCase {
     public String baseUrl = "https://www.google.com/";
     String driverPath = "/Users/majdalaamai/IdeaProjects/AutoTest1/src/test/resources/chromedriver";
     public WebDriver driver ;
